8b1d61c7089a1f86f0de93b2451436f353dbc227
Aniello Maiese <aniello.maiese@mecglobal.com>
2017-10-08 16:51:00 +0200

    added compression, cache, and delegated file parsing to backend

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from flask_compress import Compress
+from app.cache import cache
 
 app = Flask(__name__, static_url_path='/static')
+
+
 Compress(app)
+cache.init_app(app)
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,19 +1,29 @@
 from app import app
-from flask import render_template
-from collections import OrderedDict
-import csv, io, os
+from app.cache import cache
+
+from flask import render_template, send_from_directory
+from flask.json import jsonify
+
+import csv, os
 
 
-@app.route('/hello')
-def index():
-    return "Hello"
+@cache.cached(300, key_prefix='data')
+@app.route("/data")
+def data():
+    filepath = os.path.join(os.path.dirname(__file__),'static/data/Assoluti_Regione.csv')
+    with open(filepath, 'r', encoding='ISO-8859-1') as f:
+        reader = csv.DictReader(f, delimiter=";")
+        #data = [dict(x) for x in reader]
+        return jsonify(list(reader))
 
+
+@cache.cached(300, key_prefix='index')
 @app.route("/")
 def main():
+    return render_template('index.html')
 
-    filepath = os.path.join(os.path.dirname(__file__),'static/data/Assoluti_Regione.csv')
-    with io.open(filepath, 'r', encoding='ISO-8859-1') as f:
-        reader = csv.DictReader(f, delimiter=";")
-        data = [dict(x) for x in reader]
-        return render_template('index.html', data=data)
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'img/favicon.ico', mimetype='image/vnd.microsoft.icon')
