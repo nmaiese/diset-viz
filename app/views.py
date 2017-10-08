5667973697ff6393c9c602bbd9d4ce7f6af9d92d
Aniello Maiese <aniello.maiese@mecglobal.com>
2017-10-08 15:38:26 +0200

    updated to python3

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template
+from collections import OrderedDict
 import csv, io, os
+
 
 @app.route('/hello')
 def index():
@@ -10,7 +12,8 @@ def index():
 def main():
 
     filepath = os.path.join(os.path.dirname(__file__),'static/data/Assoluti_Regione.csv')
-    with io.open(filepath, 'rb') as f:
+    with io.open(filepath, 'r', encoding='ISO-8859-1') as f:
         reader = csv.DictReader(f, delimiter=";")
-        return render_template('index.html', data=list(reader))
+        data = [dict(x) for x in reader]
+        return render_template('index.html', data=data)
 
